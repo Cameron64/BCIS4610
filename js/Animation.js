6066eb8549d5a0cfe3c476fe8cd6dffd2c6554cf
Cameron64 <cameron64@ymail.com>
2014-09-04 20:47:43 -0700

    Adding Project from today's work

---
+++ js/Animation.js
@@ -0,0 +1,9 @@
+/**
+ * Created by camer_000 on 8/31/2014.
+ */
+(function(){
+    var module = angular.module("Animation",[]);
+
+
+
+})();
--- js/magic.js
+++ js/magic.js
@@ -1,3 +1,19 @@
 /**
  * Created by camer_000 on 9/3/2014.
  */
+(function() {
+
+    var app = angular.module('business', []);
+
+    app.controller('productController',function($http){
+
+        scop = this;
+        $http.get('js/array1.json').then(function(res){scop.products = res.data;});
+
+
+
+    });
+
+
+
+})();
