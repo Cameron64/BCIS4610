4a6dee6dc7ee4d55391b828ae86220c15218487d
Cameron64 <cameron64@ymail.com>
2014-09-06 18:27:42 -0700

    Added cookie function

--- js/magic.js
+++ js/magic.js
@@ -3,16 +3,33 @@
  */
 (function() {
 
-    var app = angular.module('business', ['ngAnimate']);
+    var app = angular.module('business', ['ngAnimate', 'ngCookies']);
 
-    app.controller('productController',function($http){
-
+    app.controller('productController', ['$http', '$cookieStore',  function ($http, $cookieStore) {
         scop = this;
         $http.get('js/array.json').then(function(res){scop.products = res.data;});
 
 
 
-    });
+
+        this.setValue = function() {
+
+            $cookieStore.put("cookieHolder", 'cookie');
+            console.log("doing cookies");
+            this.getValue();
+
+        };
+
+        this.getValue = function() {
+           var favoriteCookie = $cookieStore.get("cookieHolder");
+            console.log(favoriteCookie);
+        };
+
+
+
+
+
+    }]);
 
 
 
