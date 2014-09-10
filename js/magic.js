872457795d66c1a7e01be69cbcc23364f94cb861
Cameron64 <cameron64@ymail.com>
2014-09-10 14:26:32 -0500

    basket created, selections glow

--- js/magic.js
+++ js/magic.js
@@ -6,14 +6,48 @@
     var app = angular.module('business', ['ngAnimate', 'ngCookies']);
 
     app.controller('productController', ['$http', '$cookieStore',  function ($http, $cookieStore) {
+
         scop = this;
         $http.get('js/array.json').then(function(res){scop.products = res.data;});
+        this.basket = [];
+
+        this.addProduct = function(product){
+         this.basket.push(product);
+            console.log(this.basket);
+            console.log("adding to basket");
+        };
+
+
+        this.removeProduct = function(product){
+
+            this.a = this.basket.indexOf(product);
+            this.basket.splice(this.a,1);
+
+        };
+
+
+        this.select = function(id) {
+            if(document.getElementById(id).className === "boxSelected"){
+                document.getElementById(id).className = "box"
+            }
+            else{document.getElementById(id).className = "boxSelected"}
+
+            if(this.basket.indexOf(id) < 0){
+                this.addProduct(id);
+                console.log("does not contain product");
+            }
+            else{
+                this.removeProduct(id);
+            }
+
+        };
+
 
 
 
 
         this.setValue = function() {
-
+           // ng-click="productCtrl.setValue()"
             $cookieStore.put("cookieHolder", 'cookie');
             console.log("doing cookies");
             this.getValue();
@@ -24,6 +58,12 @@
            var favoriteCookie = $cookieStore.get("cookieHolder");
             console.log(favoriteCookie);
         };
+
+      
+
+
+
+
 
 
 
