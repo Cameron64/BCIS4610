8be98d82decf63d18f8197747daa03a07a71de4c
Cameron64 <cameron64@ymail.com>
2014-09-11 21:26:26 -0500

    fixed cookie issue

--- js/magic.js
+++ js/magic.js
@@ -3,22 +3,73 @@
  */
 (function() {
 
-    var app = angular.module('business', ['ngAnimate', 'ngCookies']);
+    var app = angular.module('business', ['ngAnimate']);
 
-    app.controller('productController', ['$http', '$cookieStore',  function ($http, $cookieStore) {
+    app.controller('productController', ['$http', '$timeout',  function ($http, $timeout) {
 
         scop = this;
         $http.get('js/array.json').then(function(res){scop.products = res.data;});
         this.basket = [];
 
 
+        this.checkCookie = function() {
+            //if page has no cookie, create a cookie, else, dissect the cookie and add contents into basket then populate the page
+            var cookieName=this.getCookie();
+            console.log(cookieName === "nothing");
+            if(cookieName === "nothing"){
+                this.setCookie();
+            }
+            else {
+                // else, dissect the cookie and add contents into basket
+                console.log("dissecting cookie");
+                this.basket = this.getCookie().split(",");
+                console.log("printing basket");
+                console.log(this.basket);
+
+
+
+            }
+        };
+
+
+           this.getClass = function(name){
+             if(this.basket.indexOf(name) >= 0) {
+                 return "boxSelected";
+             }
+               else{
+                 return "box";
+             }
+           };
+
+
+
+        this.getCookie = function() {
+            var name = "rollingSharpener" + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0; i<ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1);
+                if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+//                console.log(c.substring(name.length, c.length));
+            }
+            return "nothing";
+
+        };
+
+        this.setCookie = function() {
+                var d = new Date();
+                d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+                var expires = "expires=" + d.toUTCString();
+                document.cookie = "rollingSharpener" + "=" + this.basket + "; " + expires;
+        };
 
 
 
         this.addProduct = function(product){
          this.basket.push(product);
+            console.log(this.basket);
          document.getElementById(product).className = "boxSelected";
-            this.setValue();
+            this.setCookie();
         };
 
 
@@ -26,13 +77,15 @@
 
             this.a = this.basket.indexOf(product);
             this.basket.splice(this.a,1);
+            console.log(this.basket);
             document.getElementById(product).className = "box";
-            this.setValue();
+            this.setCookie();
 
         };
 
 
         this.select = function(id) {
+
 
 
             if(this.basket.indexOf(id) < 0){
@@ -43,28 +96,6 @@
             }
 
         };
-
-
-
-
-
-        this.setValue = function() {
-           // ng-click="productCtrl.setValue()"
-            $cookieStore.put("cookieHolder", this.basket);
-            this.getValue();
-
-        };
-
-        this.getValue = function() {
-           var favoriteCookie = $cookieStore.get("cookieHolder");
-            console.log(favoriteCookie);
-        };
-
-      
-
-
-
-
 
 
 
