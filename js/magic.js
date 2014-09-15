e1d287bd756c32f4d75c673205f99657b86f717e
Cameron64 <cameron64@ymail.com>
2014-09-14 21:07:51 -0500

    Removed unused directive

--- js/magic.js
+++ js/magic.js
@@ -5,27 +5,6 @@
 
     var app = angular.module('business', ['ngAnimate']);
 
-    app.directive( 'goClick', function ( $location ) {
-        return function ( scope, element, attrs ) {
-            var path;
-
-            attrs.$observe( 'goClick', function (val) {
-                path = val;
-            });
-
-            element.bind( 'click', function () {
-                scope.$apply( function () {
-                    $location.path( path );
-                });
-            });
-        };
-    });
-
-    app.controller('MainCtrl', function($scope, $location) {
-        $scope.$watch( function () { return $location.path(); }, function (path) {
-            $scope.path = path;
-        });
-    });
 
     app.controller('productController', ['$http', function ($http) {
 
