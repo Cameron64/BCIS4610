c834337a98dd44a5eda3bc1b1e08e7fbe3dd82a7
Cameron64 <cameron64@ymail.com>
2014-10-06 16:11:16 -0500

    Changed cookie Expiration Date

--- js/magic.js
+++ js/magic.js
@@ -100,7 +100,8 @@
 * expires in 10 days*/
         this.setCookie = function() {
                 var d = new Date();
-                d.setTime(d.getTime() + (10 * 24 * 60 * 60 * 1000));
+            //Cookie for 10 years
+                d.setTime(d.getTime() + (3600 * 1000 * 24 * 365 * 10));
                 var expires = "expires=" + d.toUTCString();
                 document.cookie = "rollingSharpener" + "=" + this.basket + "; " + expires;
         };
