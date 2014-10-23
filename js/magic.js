a22eb5f727ebc4d808ab6cd62d0a9a818d9431c7
Cameron64 <cameron64@ymail.com>
2014-10-23 12:04:07 -0500

    fixed basket positioning

--- js/magic.js
+++ js/magic.js
@@ -80,6 +80,21 @@
                     /*need to make watcher run more frequently than on a click*/
         }}
             );
+        $scope.$watch(function(){
+                var name = "rollingSharpener" + "=";
+                var ca = document.cookie.split(';');
+                for (var i = 0; i < ca.length; i++) {
+                    var c = ca[i];
+                    while (c.charAt(0) == ' ') c = c.substring(1);
+                    if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+                }
+                return "nothing";}, function(newValue, oldValue){
+                if(newValue != oldValue){
+
+                    scop.checkCookie();
+                    /*need to make watcher run more frequently than on a click*/
+                }}
+        );
 
 
         this.initQuantities = function(){
@@ -222,9 +237,6 @@
 
     }]);
 
-    app.controller("ReviewController", function () {
 
-
-    });
 
 })();
