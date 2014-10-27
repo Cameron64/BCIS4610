5c076cf9f3c7b5c60563d8f53d168e21ab332565
Cameron64 <cameron64@ymail.com>
2014-10-26 21:05:54 -0500

    introducing modals

--- js/magic.js
+++ js/magic.js
@@ -3,7 +3,7 @@
  */
 (function () {
 
-    var app = angular.module('business', ['ngAnimate']);
+    var app = angular.module('business', ['ngAnimate','ui.bootstrap']);
 
 
     app.controller('productController', ['$http','$scope','$document', function ($http,$scope,$document) {
@@ -256,6 +256,48 @@
 
     }]);
 
+app.controller('ModalDemoCtrl', function ($scope, $modal, $log) {
 
+    $scope.items = ['item1', 'item2', 'item3'];
+
+    $scope.open = function (size) {
+
+        var modalInstance = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalInstanceCtrl',
+            size: size,
+            resolve: {
+                items: function () {
+                    return $scope.items;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+        }, function () {
+            $log.info('Modal dismissed at: ' + new Date());
+        });
+    };
+});
+
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+
+    app.controller('ModalInstanceCtrl', function ($scope, $modalInstance, items) {
+
+        $scope.items = items;
+        $scope.selected = {
+            item: $scope.items[0]
+        };
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.selected.item);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    });
 
 })();
